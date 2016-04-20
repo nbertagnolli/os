12fda54aafa2e4a787270599e10046c2ee8d4391
nbertagnolli <bertagno@cs.utah.edu>
2016-04-20 12:29:51 -0600

    added sleepy

--- hw05/sleepy.c
+++ hw05/sleepy.c
@@ -148,6 +148,7 @@ sleepy_write(struct file *filp, const char __user *buf, size_t count,
     // __user is the input
     printk(KERN_DEBUG "process %i (%s) sleeping devices\n", current->pid, current->comm);
     printk(KERN_DEBUG "Device ID %d\n", dev->id);
+    
     /*
     int input;
     input = 1;
